f208f65e1a16b9109c720974efb64e6541915f29
Decebal Dobrica <352761+decebal@users.noreply.github.com>
2026-07-17 21:17:10 +0100

    chore: bump version to 0.4.0 and add changelog

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule McpLogServer.MixProject do
   def project do
     [
       app: :mcp_log_server,
-      version: "0.1.0",
+      version: "0.4.0",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
